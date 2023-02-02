25b7062462ea57088fc0ce430c098ffd326f5e04
msi <it@megroupco.net>
2023-02-02 14:32:44 +0200

    last

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,7 @@ import 'Features/Screens/SplachScreen.dart';
 import 'Features/Supervisor/Home/SHomeProvider.dart';
 import 'Features/Supervisor/WorkAndBill/SWorkAndBillProvider.dart';
 
-//abdallah
-//asd
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
